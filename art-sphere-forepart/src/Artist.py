12ab62b56842184c57cdf4a8621c1e784196b00e
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-08-03 05:57:52 +0300

    Added Axios for API requests and form handling

---
+++ art-sphere-forepart/src/Artist.py
@@ -0,0 +1,13 @@
+import React, { userState } from 'eact';
+import axios from 'axios'
+
+const Artist = () => {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('');
+    const [image_url, setImageUrl] = useState('');
+    const [price, setPrice] = useState(0);
+    const [genre, setGenre] = useState('');
+
+    
+    }
+}
